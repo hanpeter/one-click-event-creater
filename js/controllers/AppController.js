07cac9b5169169ba09fe29c14425c9ae37d53baa
Peter Han <han.peter.621@gmail.com>
2015-09-01 23:08:50 -0700

    More test code on posting events Code cleanup

--- js/controllers/AppController.js
+++ js/controllers/AppController.js
@@ -1,30 +1,34 @@
 App.controller('AppController', ['$scope', '$timeout', 'CalendarService', function ($scope, $timeout, CalendarService) {
     _.extend($scope, {
-        selectedCalendarIndex: 0,
         calendars: []
     });
 
     CalendarService.getCalendarLists()
-        .then(function (data) {
-            _.forEach(data.items, function (cal, index) {
+        .then(function (calendarList) {
+            _.forEach(calendarList, function (cal, index) {
                 var calendar = {
                     id: cal.id,
                     name: cal.summary,
                     description: cal.description,
-                    events: []
+                    events: {}
                 };
                 $scope.calendars[index] = calendar;
                 CalendarService.getCalendarEvents(calendar.id)
-                    .then(function (data) {
-                        _.forEach(data.items, function (e) {
+                    .then(function (events) {
+                        _.forEach(events, function (e) {
                             var event = {
                                 id: e.id,
                                 name: e.summary,
-                                start: new Date(e.start.dateTime),
-                                end: new Date(e.end.dateTime),
+                                start: moment(e.start.dateTime || e.start.date),
+                                end: moment(e.end.dateTime || e.end.date),
                                 attendees: []
                             };
-                            calendar.events.push(event);
+                            var day = event.start.format('YYYY-MM-DD');
+
+                            if (!calendar.events[day]) {
+                                calendar.events[day] = [];
+                            }
+                            calendar.events[day].push(event);
 
                             _.forEach(e.attendees, function (attendee) {
                                 event.attendees.push({
@@ -34,6 +38,22 @@ App.controller('AppController', ['$scope', '$timeout', 'CalendarService', functi
                                 });
                             });
                         });
+
+                        CalendarService.createCalendarEvent({
+                            attendees: [{
+                                email: 'han.peter.621@gmail.com'
+                            }, {
+                                email: 'phan@clicktime.com'
+                            }],
+                            start: {
+                                dateTime: moment()
+                            },
+                            end: {
+                                dateTime: moment().add(1, 'h')
+                            },
+                            summary: 'Test Event',
+                            description: 'Description'
+                        }, $scope.calendars[0].id);
                     });
             });
         });
--- js/services/CalendarService.js
+++ js/services/CalendarService.js
@@ -1,18 +1,33 @@
 App.service('CalendarService', ['$http', function ($http) {
     var me = this;
     var getCalendarEventsTemplate = _.template('https://www.googleapis.com/calendar/v3/calendars/<% print(encodeURIComponent(id)); %>/events');
+    var addCalendarEventsTemplate = _.template('https://www.googleapis.com/calendar/v3/calendars/<% print(encodeURIComponent(id)); %>/events');
 
     _.extend(me, {
         getCalendarLists: function () {
             return $http({
                 url: 'https://www.googleapis.com/calendar/v3/users/me/calendarList',
             }).then(function (data) {
-                return data.data;
+                return _.filter(data.data.items, function (calendar) {
+                    return calendar.id.indexOf('@group.v.calendar.google.com') < 0;
+                });
             });
         },
         getCalendarEvents: function (id) {
             return $http({
                 url: getCalendarEventsTemplate({ id: id }),
+            }).then(function (data) {
+                return data.data.items;
+            });
+        },
+        createCalendarEvent: function (event, calendarID) {
+            return $http({
+                url: addCalendarEventsTemplate({ id: calendarID }),
+                method: 'POST',
+                params: {
+                    sendNotifications: true
+                },
+                data: event
             }).then(function (data) {
                 return data.data;
             });
