7a24f5e739b431f586b755838d459d352e5199fe
Peter Han <han.peter.621@gmail.com>
2015-09-21 02:47:32 -0700

    Code refactoring

--- js/controllers/AppController.js
+++ js/controllers/AppController.js
@@ -1,4 +1,4 @@
-App.controller('AppController', ['$scope', '$timeout', 'CalendarService', 'StorageService', function ($scope, $timeout, CalendarService, StorageService) {
+App.controller('AppController', ['$scope', '$timeout', '$q', 'CalendarService', 'StorageService', function ($scope, $timeout, $q, CalendarService, StorageService) {
     _.extend($scope, {
         calendars: [],
         primaryCalendar: null,
@@ -37,52 +37,31 @@ App.controller('AppController', ['$scope', '$timeout', 'CalendarService', 'Stora
 
     CalendarService.getCalendarLists()
         .then(function (calendarList) {
-            _.forEach(calendarList, function (cal, index) {
-                var calendar = {
-                    id: cal.id,
-                    name: cal.summary,
-                    description: cal.description,
-                    events: {}
-                };
-                $scope.calendars[index] = calendar;
+            $scope.calendars = calendarList;
 
-                if (cal.primary) {
-                    $scope.primaryCalendar = calendar;
-                }
+            $scope.primaryCalendar = _.find($scope.calendars, { isPrimary: true });
 
+            return $scope.calendars;
+        })
+        .then(function (calendarList) {
+            return $q.all(_.map(calendarList, function (calendar) {
                 CalendarService.getCalendarEvents(calendar.id)
                     .then(function (events) {
-                        _.forEach(events, function (e) {
-                            var event = {
-                                id: e.id,
-                                name: e.summary,
-                                start: moment(e.start.dateTime || e.start.date),
-                                end: moment(e.end.dateTime || e.end.date),
-                                attendees: []
-                            };
-                            var day = event.start.format('YYYY-MM-DD');
-
-                            if (!calendar.events[day]) {
-                                calendar.events[day] = [];
-                            }
-                            calendar.events[day].push(event);
-
-                            _.forEach(e.attendees, function (attendee) {
-                                event.attendees.push({
-                                    email: attendee.email,
-                                    name: attendee.displayName,
-                                    status: attendee.responseStatus
-                                });
-                            });
-                        });
+                        calendar.events = events;
                     });
-            });
+            }));
+        })
+        .then(function () {
+            console.log($scope.calendars);
         });
 
     StorageService.getTemplates()
         .then(function (templates) {
             $scope.templates = templates;
             $scope.selectedTemplate = templates[0];
+        })
+        .then(function () {
+            console.log($scope.templates);
         });
 
     $timeout(function () {
--- js/services/CalendarService.js
+++ js/services/CalendarService.js
@@ -6,31 +6,76 @@ App.service('CalendarService', ['$http', function ($http) {
     _.extend(me, {
         getCalendarLists: function () {
             return $http({
-                url: 'https://www.googleapis.com/calendar/v3/users/me/calendarList',
-            }).then(function (data) {
-                return _.filter(data.data.items, function (calendar) {
-                    return calendar.id.indexOf('@group.v.calendar.google.com') < 0;
+                    url: 'https://www.googleapis.com/calendar/v3/users/me/calendarList',
+                })
+                .then(function (data) {
+                    return _.filter(data.data.items, function (calendar) {
+                        return calendar.id.indexOf('@group.v.calendar.google.com') < 0;
+                    });
+                })
+                .then(function (calendarList) {
+                    return _.map(calendarList, function (cal) {
+                        return {
+                            id: cal.id,
+                            name: cal.summary,
+                            description: cal.description,
+                            events: {},
+                            isPrimary: cal.primary
+                        };
+                    });
                 });
-            });
         },
         getCalendarEvents: function (id) {
             return $http({
-                url: getCalendarEventsTemplate({ id: id }),
-            }).then(function (data) {
-                return data.data.items;
-            });
+                    url: getCalendarEventsTemplate({ id: id }),
+                })
+                .then(function (data) {
+                    return data.data.items;
+                })
+                .then(function (events) {
+                    return _.map(events, function (e) {
+                        var attendees = _.map(e.attendees, function (attendee) {
+                            return {
+                                email: attendee.email,
+                                name: attendee.displayName,
+                                status: attendee.responseStatus
+                            };
+                        });
+
+                        return {
+                            id: e.id,
+                            name: e.summary,
+                            start: moment(e.start.dateTime || e.start.date),
+                            end: moment(e.end.dateTime || e.end.date),
+                            attendees: attendees
+                        };
+                    });
+                })
+                .then(function (events) {
+                    return _.reduce(events, function (result, event) {
+                        var day = event.start.format('YYYY-MM-DD');
+
+                        if (!result[day]) {
+                            result[day] = [];
+                        }
+                        result[day].push(event);
+
+                        return result;
+                    }, {});
+                });
         },
         createCalendarEvent: function (event, calendarID) {
             return $http({
-                url: addCalendarEventsTemplate({ id: calendarID }),
-                method: 'POST',
-                params: {
-                    sendNotifications: true
-                },
-                data: event
-            }).then(function (data) {
-                return data.data;
-            });
+                    url: addCalendarEventsTemplate({ id: calendarID }),
+                    method: 'POST',
+                    params: {
+                        sendNotifications: true
+                    },
+                    data: event
+                })
+                .then(function (data) {
+                    return data.data;
+                });
         }
     });
 }]);
