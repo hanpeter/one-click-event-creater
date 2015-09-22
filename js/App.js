0ae8dc5a582f164d8775d3df5a96f3e28f4eebe7
Peter Han <han.peter.621@gmail.com>
2015-09-22 01:48:46 -0700

    Rollbar setup

--- js/App.js
+++ js/App.js
@@ -1,4 +1,14 @@
-window.App = angular.module('App', [])
+window.App = angular.module('App', ['rollbar'])
     .config(['$compileProvider', function ($compileProvider) {   
         $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension):/);
+    }])
+    .config(['$rollbarProvider', function ($rollbarProvider) {
+        $rollbarProvider.init({
+            accessToken: '435b6b3b249841d4ba71d7c5fad60ea1',
+            verbose: true,
+            captureUncaught: true,
+            payload: {
+                environment: 'production'
+            }
+        });
     }]);
